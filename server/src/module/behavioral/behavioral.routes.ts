d72ae2827499e706d6c27372fbdbdab9a3c7fece
Akshita <akpandey2307@gmail.com>
2026-07-30 10:36:59 +0530

    fix: gate behavioral evaluation behind premium subscription (#2887)

--- server/src/module/behavioral/behavioral.routes.ts
+++ server/src/module/behavioral/behavioral.routes.ts
@@ -4,6 +4,7 @@ import { BehavioralService } from "./behavioral.service.js";
 import { authMiddleware } from "../../middleware/auth.middleware.js";
 import { requireRole } from "../../middleware/role.middleware.js";
 import { usageLimit } from "../../middleware/usage-limit.middleware.js";
+import { requirePremium } from "../../middleware/premium.middleware.js";
 
 const behavioralService = new BehavioralService();
 const behavioralController = new BehavioralController(behavioralService);
@@ -12,4 +13,4 @@ export const behavioralRouter = Router();
 
 behavioralRouter.use(authMiddleware, requireRole("STUDENT"));
 
-behavioralRouter.post("/evaluate", usageLimit("BEHAVIORAL_EVAL"), (req, res, next) => behavioralController.evaluate(req, res, next));
+behavioralRouter.post("/evaluate", usageLimit("BEHAVIORAL_EVAL"), requirePremium("behavioral evaluation"), (req, res, next) => behavioralController.evaluate(req, res, next));
